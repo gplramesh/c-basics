6e33175b31a5970f81002dab5681cf5baa4fecb0
gplramesh <93215922+gplramesh@users.noreply.github.com>
2021-11-09 10:57:55 +0530

    Add files via upload

--- 123.c
+++ 123.c
@@ -1,12 +1,8 @@
 #include<stdio.h>
-int main()
+void main()
 {
-	int n,e,o,s;
-	n=5;
-	s=n*(n+1)/2;
-	e=n*(n+1);
-	o=n*n;
-	printf("sum of %d natural number is %d\n",n,s);
-	printf("sum of %d even number is %d\n",n,e);
-	printf("sum of %d odd number is %d\n",n,o);
+	int n;
+	printf("enter a number(from 1-26)");
+	scanf("%d",&n);
+	printf("%c",n+96);
 }
