c092623f8ef05712b8f4ffe83e5a78a57f71f85d
gplramesh <93215922+gplramesh@users.noreply.github.com>
2021-11-09 10:57:07 +0530

    Add files via upload

---
+++ reading.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main()
+{
+	int a,b,c;
+	printf("enter 1st number",a);
+	scanf("%d",&a);
+	printf("enter 2nd number",b);
+	scanf("%d",&b);
+	printf("enter 3rd number",c);
+	scanf("%d",&c);
+	printf("number1:%d\n",a);
+	printf("number2:%d\n",b);
+	printf("number3:%d\n",c);
+}
