8e29d5796c60701cf6bb6c08d30de4ea83014b5b
gplramesh <93215922+gplramesh@users.noreply.github.com>
2021-11-16 10:47:15 +0530

    Add files via upload

---
+++ asiic  RAMESH.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main()
+{
+	int a,b,c,d,e,f;
+	printf("enter six  numbers");
+	scanf("%d%d%d%d%d%d",&a,&b,&c,&d,&e,&f);
+	printf("%c%c%c%c%c%c",a+64,b+64,c+64,d+64,e+64,f+64 );
+}
