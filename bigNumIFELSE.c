101b56b9bdb3c18a365c47a7ac4df33e3df0f806
gplramesh <93215922+gplramesh@users.noreply.github.com>
2021-12-08 10:01:20 +0530

    Add files via upload

---
+++ bigNumIFELSE.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+void main()
+{
+	int a,b;
+	printf("enter the numbers");
+	scanf("%d%d",&a,&b);
+	if(a>b)
+	{
+		printf("the big number is%d",a);
+		
+	}
+	else
+	{
+	printf("the bigest number is%d",b);
+	}
+}
+
+
