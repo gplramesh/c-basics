e334e6c7ac8200255378aefca6549b86d02d9180
gplramesh <93215922+gplramesh@users.noreply.github.com>
2021-12-08 10:04:09 +0530

    Add files via upload

---
+++ week day.c
@@ -0,0 +1,59 @@
+#include<stdio.h>
+void main()
+{
+	int a;
+	printf("enter the number");
+	scanf("%d",&a);
+	if(a==1)
+	{
+		printf("jan",a);
+	}
+	else if(a==2)
+	{
+		printf("feb",a);
+	}
+	else if(a==3)
+	{
+		printf("mar",a);
+	}
+	else if(a==4)
+	{
+		printf("april",a);
+	}
+	else if(a==5)
+	{
+		printf("may",a);
+	}
+	else if(a==6)
+	{
+		printf("jun",a);
+	}
+	else if(a==7)
+	{
+		printf("july",a);
+	}
+		else if(a==8)
+	{
+		printf("aug",a);
+	}
+		else if(a==9)
+	{
+		printf("sep",a);
+	}
+		else if(a==10)
+	{
+		printf("oct",a);
+	}
+		else if(a==11)
+	{
+		printf("nov",a);
+	}
+		else if(a==12)
+	{
+		printf("dec",a);
+	}
+	else
+	{
+		printf("invaild");
+	}
+}
